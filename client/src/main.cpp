e768632ff5d5231cfb940b66a02287dc1ea7132e
Sam Smith <sammys.email.home@gmail.com>
2025-11-21 14:49:41 -0600

    Small fixes to UI

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -21,263 +21,268 @@ using tcp = boost::asio::ip::tcp;
 using namespace ftxui;
 
 std::string getCurrDir() {
-  try {
-    boost::filesystem::path currentPath = boost::filesystem::current_path();
-    return currentPath.string();
-  } catch (const boost::filesystem::filesystem_error &ex) {
-    return ex.what();
-  }
+    try {
+        boost::filesystem::path currentPath = boost::filesystem::current_path();
+        return currentPath.string();
+    } catch (const boost::filesystem::filesystem_error &ex) {
+        return ex.what();
+    }
 }
 
 struct Config {
-  std::string host = DEFAULT_HOST;
-  std::string port = DEFAULT_PORT;
-  std::string target = "/";
-  std::string root_fs = getCurrDir() + "/troot";
-  int https = 0;
-  std::string sync_period = "30000";
+    std::string host = DEFAULT_HOST;
+    std::string port = DEFAULT_PORT;
+    std::string target = "/";
+    std::string root_fs = getCurrDir() + "/troot";
+    int https = 0;
+    std::string sync_period = "30000";
 };
 
 struct AppState {
-  Config cfg;
-  Config temp; // temporary config
+    Config cfg;
+    Config temp; // temporary config
 
-  // Shared string component
-  std::string bt_request_message = "Press 's' to sync with tracker";
-  std::string status = "";
-  std::string help_message =
-      "Press 'o' for options.\nPress 's' to sync.\nPress 'q' to quit.";
+    // Shared string component
+    std::string bt_request_message = "Press 's' to sync with tracker";
+    std::string status = "";
+    std::string help_message =
+        "Press 'o' for options.\nPress 's' to sync.\nPress 'q' to quit.";
 
-  // Modal state
-  bool show_options = false;
-  std::string error_msg;
+    // Modal state
+    bool show_options = false;
+    std::string error_msg;
 
-  // Table data headers
-  std::vector<TorrentEntry> torrent_entries;
-  std::vector<std::string> schemes = {"http", "https"};
-  std::atomic<bool> busy = false;
+    // Table data headers
+    std::vector<TorrentEntry> torrent_entries;
+    std::vector<std::string> schemes = {"http", "https"};
+    std::atomic<bool> busy = false;
 };
 
 Element boolDecorator(Element child, bool synced) {
-  if (synced) {
-    return child | color(Color::Green);
-  } else {
-    return child | color(Color::Red);
-  }
+    if (synced) {
+        return child | color(Color::Green);
+    } else {
+        return child | color(Color::Red);
+    }
+}
+
+Element RenderMainView(AppState &state) {
+    auto sec = state.cfg.https ? "https" : "http";
+
+    return window(text("BT-Mini Client") | bold,
+                  vbox({vbox({text("Configured endpoint: " + std::string(sec) +
+                                   "://" + state.temp.host + ":" +
+                                   state.temp.port + state.temp.target),
+                              separator(), paragraph(state.help_message)})}));
 }
 
 Element RenderSyncTable(AppState &state) {
+    std::vector<std::vector<std::string>> table_data;
+    table_data.push_back({"Name", "Size (B)", "Infohash", "Synced?"});
 
-  std::vector<std::vector<std::string>> table_data;
-  table_data.push_back({"Name", "Size (B)", "Infohash", "Synced?"});
+    for (const TorrentEntry &te : state.torrent_entries) {
+        std::string size = std::to_string(te.size_bytes);
+        std::string synced = te.synced ? "True" : "False";
 
-  for (const TorrentEntry &te : state.torrent_entries) {
-    std::string size = std::to_string(te.size_bytes);
-    std::string synced = te.synced ? "True" : "False";
-
-    table_data.push_back({te.name, size, te.infohash, synced});
-  }
-
-  Table table(table_data);
-
-  table.SelectAll().Border();
-  table.SelectRow(0).Separator();
-  table.SelectAll().SeparatorHorizontal();
-
-  // This is to decorate the synced column to be red for false and green for
-  // true!
-  for (size_t row = 1; row < table_data.size(); ++row) {
-    if (3 >= (int)table_data[row].size()) {
-      continue;
+        table_data.push_back({te.name, size, te.infohash, synced});
     }
 
-    bool is_synced = table_data[row][3].compare("True") == 0;
+    Table table(table_data);
 
-    table.SelectRow(row).DecorateCells([is_synced](Element cell) {
-      return boolDecorator(std::move(cell), is_synced);
-    });
-  }
+    table.SelectAll().Border();
+    table.SelectRow(0).Separator();
+    table.SelectAll().SeparatorHorizontal();
 
-  Element table_el = table.Render() | size(WIDTH, LESS_THAN, 100) |
-                     size(HEIGHT, LESS_THAN, 50);
+    // This is to decorate the synced column to be red for false and green
+    // for true!
+    for (size_t row = 1; row < table_data.size(); ++row) {
+        if (3 >= (int)table_data[row].size()) {
+            continue;
+        }
 
-  return window(text("Sync Table") | bold, vbox({table_el})) | center;
+        bool is_synced = table_data[row][3].compare("True") == 0;
+
+        table.SelectRow(row).DecorateCells([is_synced](Element cell) {
+            return boolDecorator(std::move(cell), is_synced);
+        });
+    }
+
+    Element table_el = table.Render() | size(WIDTH, LESS_THAN, 100) |
+                       size(HEIGHT, LESS_THAN, 50);
+
+    return window(text("Sync Table") | bold, vbox({table_el})) | center;
 };
 
 /// Copies temp options to main config
 void cp_options(AppState &state) {
-  state.cfg.host = state.temp.host;
-  state.cfg.port = state.temp.port;
-  state.cfg.target = state.temp.target;
-  state.cfg.https = state.temp.https;
-  state.cfg.root_fs = state.temp.root_fs;
-  state.cfg.sync_period = state.temp.sync_period;
+    state.cfg.host = state.temp.host;
+    state.cfg.port = state.temp.port;
+    state.cfg.target = state.temp.target;
+    state.cfg.https = state.temp.https;
+    state.cfg.root_fs = state.temp.root_fs;
+    state.cfg.sync_period = state.temp.sync_period;
 }
 
 /// Resets temp options to main config
 void rst_options(AppState &state) {
-  state.temp.host = state.cfg.host;
-  state.temp.port = state.cfg.port;
-  state.temp.target = state.cfg.target;
-  state.temp.https = state.cfg.https;
-  state.temp.root_fs = state.cfg.root_fs;
-  state.temp.sync_period = state.cfg.sync_period;
+    state.temp.host = state.cfg.host;
+    state.temp.port = state.cfg.port;
+    state.temp.target = state.cfg.target;
+    state.temp.https = state.cfg.https;
+    state.temp.root_fs = state.cfg.root_fs;
+    state.temp.sync_period = state.cfg.sync_period;
 }
 
 Component MakeOptionsModal(AppState &state, ScreenInteractive &screen) {
+    // Inputs
+    auto input_host = Input(&state.temp.host, "hostname");
+    auto input_port = Input(&state.temp.port, "port");
+    auto input_target = Input(&state.temp.target, "path");
+    auto root_input = Input(&state.temp.root_fs, "path");
+    auto input_sync_p = Input(&state.temp.sync_period, "milliseconds");
+    auto sec_toggle = Toggle(state.schemes, &state.temp.https);
 
-  // Inputs
-  auto input_host = Input(&state.temp.host, "hostname");
-  auto input_port = Input(&state.temp.port, "port");
-  auto input_target = Input(&state.temp.target, "path");
-  auto root_input = Input(&state.temp.root_fs, "path");
-  auto input_sync_p = Input(&state.temp.sync_period, "milliseconds");
-  auto sec_toggle = Toggle(state.schemes, &state.temp.https);
+    auto btn_ok = Button(" OK ", [&] {
+        cp_options(state);
+        state.error_msg.clear();
+        state.status = "Saved options.";
+        state.show_options = false;
+    });
 
-  auto btn_ok = Button(" OK ", [&] {
-    cp_options(state);
-    state.error_msg.clear();
-    state.status = "Saved options.";
-    state.show_options = false;
-  });
+    auto btn_cancel = Button(" Cancel ", [&] {
+        rst_options(state);
+        state.error_msg.clear();
+        state.status = "Cancelled.";
+        state.show_options = false;
+    });
 
-  auto btn_cancel = Button(" Cancel ", [&] {
-    rst_options(state);
-    state.error_msg.clear();
-    state.status = "Cancelled.";
-    state.show_options = false;
-  });
+    Components form_children = {
+        input_host,
+        input_port,
+        input_target,
+        sec_toggle,
+        root_input,
+        input_sync_p,
+        Container::Horizontal(Components{btn_ok, btn_cancel})};
 
-  Components form_children = {
-      input_host,
-      input_port,
-      input_target,
-      sec_toggle,
-      root_input,
-      input_sync_p,
-      Container::Horizontal(Components{btn_ok, btn_cancel})};
+    Component modal_form = Container::Vertical(form_children);
 
-  Component modal_form = Container::Vertical(form_children);
+    Component modal_window = Renderer(
+        modal_form,
+        [&, input_host, input_port, input_target, sec_toggle, root_input,
+         input_sync_p, btn_ok, btn_cancel]() -> Element {
+            Element err = state.error_msg.empty()
+                              ? filler()
+                              : text(state.error_msg) | color(Color::RedLight);
 
-  Component modal_window = Renderer(
-      modal_form,
-      [&, input_host, input_port, input_target, sec_toggle, root_input,
-       input_sync_p, btn_ok, btn_cancel]() -> Element {
-        Element err = state.error_msg.empty()
-                          ? filler()
-                          : text(state.error_msg) | color(Color::RedLight);
+            return vbox(text(" Options ") | bold | center, separator(),
+                        text("Network Settings"),
+                        hbox(text(" Host     ") | dim, input_host->Render()) |
+                            size(WIDTH, EQUAL, 48),
+                        hbox(text(" Port     ") | dim, input_port->Render()) |
+                            size(WIDTH, EQUAL, 48),
+                        hbox(text(" Target   ") | dim, input_target->Render()) |
+                            size(WIDTH, EQUAL, 48),
+                        hbox(text(" Scheme   ") | dim, sec_toggle->Render()) |
+                            size(WIDTH, EQUAL, 48),
 
-        return vbox(text(" Options ") | bold | center, separator(),
-                    text("Network Settings"),
-                    hbox(text(" Host     ") | dim, input_host->Render()) |
-                        size(WIDTH, EQUAL, 48),
-                    hbox(text(" Port     ") | dim, input_port->Render()) |
-                        size(WIDTH, EQUAL, 48),
-                    hbox(text(" Target   ") | dim, input_target->Render()) |
-                        size(WIDTH, EQUAL, 48),
-                    hbox(text(" Scheme   ") | dim, sec_toggle->Render()) |
-                        size(WIDTH, EQUAL, 48),
+                        separator(), text("Torrent Settings"),
+                        hbox(text(" Root dir ") | dim, root_input->Render()) |
+                            size(WIDTH, EQUAL, 48),
+                        hbox(text(" Sync Period ") | dim,
+                             input_sync_p->Render()) |
+                            size(WIDTH, EQUAL, 48),
+                        separator(), err, separator(),
+                        hbox(filler(), btn_ok->Render(), text("  "),
+                             btn_cancel->Render(), filler())) |
+                   border | center;
+        });
 
-                    separator(), text("Torrent Settings"),
-                    hbox(text(" Root dir ") | dim, root_input->Render()) |
-                        size(WIDTH, EQUAL, 48),
-                    hbox(text(" Sync Period ") | dim, input_sync_p->Render()) |
-                        size(WIDTH, EQUAL, 48),
-                    separator(), err, separator(),
-                    hbox(filler(), btn_ok->Render(), text("  "),
-                         btn_cancel->Render(), filler())) |
-               border | center;
-      });
-
-  return modal_window;
+    return modal_window;
 }
 
 int main(int argc, char **argv) {
-  // Keeps it cenetered, clears screen, draws to alternative screen buffer
-  auto screen = ScreenInteractive::FullscreenAlternateScreen();
+    // Keeps it cenetered, clears screen, draws to alternative screen buffer
+    auto screen = ScreenInteractive::FullscreenAlternateScreen();
 
-  // Global state
-  AppState state;
-  rst_options(state);
+    // Global state
+    AppState state;
+    rst_options(state); // Set defaults
+    state.torrent_entries = scan_root_for_torrents(
+        state.cfg.root_fs); // Setup files before rendering
 
-  // Renderer
-  Component main_view = Renderer([&] {
-    auto sec = state.cfg.https ? "https" : "http";
+    // Renderer
+    Component main_view = Renderer([&] {
+        Element status_line = hbox({text(state.status)});
+        Element main_panel =
+            vbox({RenderMainView(state), RenderSyncTable(state)});
 
-    Element status_line = hbox({text(state.status)});
+        return vbox({status_line, separator(), main_panel});
+    });
 
-    Element main_panel =
-        hbox({vbox({vbox({text("BT-Mini Client") | bold, separator(),
-                          text("Configured endpoint: " + std::string(sec) +
-                               "://" + state.temp.host + ":" + state.temp.port +
-                               state.temp.target),
-                          separator(), paragraph(state.help_message)}),
-                    vbox(text(state.bt_request_message))}) |
-                  border | center,
-              RenderSyncTable(state)});
+    Component options_modal = MakeOptionsModal(state, screen);
+    Component app = Modal(main_view, options_modal, &state.show_options);
 
-    return vbox({status_line, separator(), main_panel});
-  });
+    // Event handler
+    app = CatchEvent(app, [&](Event e) {
+        if (e == Event::Character('q')) {
+            screen.Exit();
+            return true;
+        }
+        if (e == Event::Escape) {
+            // Exit options without saving
+            state.status = "Cancelled.";
+            rst_options(state);
+            state.show_options = false;
+            return true;
+        }
+        if (e == Event::Character('o') && !state.show_options) {
+            state.show_options = true;
+            return true;
+        }
+        if (!state.show_options && e == Event::Character('s')) {
 
-  Component options_modal = MakeOptionsModal(state, screen);
-  Component app = Modal(main_view, options_modal, &state.show_options);
+            std::thread([&] {
+                screen.Post([&] {
+                    state.torrent_entries =
+                        scan_root_for_torrents(state.cfg.root_fs);
+                    state.status =
+                        "Scanned root: " + state.cfg.root_fs + " (" +
+                        std::to_string(state.torrent_entries.size()) +
+                        " files)";
+                });
 
-  // Event handler
-  app = CatchEvent(app, [&](Event e) {
-    if (e == Event::Character('q')) {
-      screen.Exit();
-      return true;
-    }
-    if (e == Event::Escape) {
-      // Exit options without saving
-      state.status = "Cancelled.";
-      rst_options(state);
-      state.show_options = false;
-      return true;
-    }
-    if (e == Event::Character('o') && !state.show_options) {
-      state.show_options = true;
-      return true;
-    }
-    if (!state.show_options && e == Event::Character('s')) {
+                screen.PostEvent(Event::Custom);
+            }).detach();
 
-      std::thread([&] {
-        screen.Post([&] {
-          state.torrent_entries = scan_root_for_torrents(state.cfg.root_fs);
-          state.status = "Scanned root: " + state.cfg.root_fs + " (" +
-                         std::to_string(state.torrent_entries.size()) +
-                         " files)";
-        });
+            //       // To make sure
+            //       if (state.busy)
+            //         return true;
+            //
+            //       state.busy = true;
+            //       state.status = "Fetching client list...";
+            //       std::thread([&] {
+            //         // Send request
+            //         std::string result = HttpGet("127.0.0.1",
+            //         DEFAULT_PORT,
+            //         "/");
+            //
+            //         // Setup lambda on UI thread queue for excecution
+            //         screen.Post([&, result = std::move(result)] {
+            //           state.status = "Client list fetched.";
+            //           state.bt_request_message = std::move(result);
+            //           state.busy = false;
+            //         });
+            //
+            //         // This just makes sure the screen wakes up and
+            //         rerenders screen.PostEvent(Event::Custom);
+            //       }).detach();
+            //
+            return true;
+        }
+        return false;
+    });
 
-        screen.PostEvent(Event::Custom);
-      }).detach();
-
-      //       // To make sure
-      //       if (state.busy)
-      //         return true;
-      //
-      //       state.busy = true;
-      //       state.status = "Fetching client list...";
-      //       std::thread([&] {
-      //         // Send request
-      //         std::string result = HttpGet("127.0.0.1", DEFAULT_PORT, "/");
-      //
-      //         // Setup lambda on UI thread queue for excecution
-      //         screen.Post([&, result = std::move(result)] {
-      //           state.status = "Client list fetched.";
-      //           state.bt_request_message = std::move(result);
-      //           state.busy = false;
-      //         });
-      //
-      //         // This just makes sure the screen wakes up and rerenders
-      //         screen.PostEvent(Event::Custom);
-      //       }).detach();
-      //
-      return true;
-    }
-    return false;
-  });
-
-  screen.Loop(app);
-  return 0;
+    screen.Loop(app);
+    return 0;
 }
